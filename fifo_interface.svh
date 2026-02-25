38bd3b2b5127f5e33916312f8e0863c73e5a6dda
arm7 <199115150+sunnysudheer393@users.noreply.github.com>
2026-02-25 11:46:48 -0500

    Refactor fifo_interface with updated clocking blocks
    Updated the fifo_interface to include a modified clocking block for the monitor and commented out the driver clocking block.

--- fifo_interface.svh
+++ fifo_interface.svh
@@ -8,25 +8,23 @@ interface fifo_interface #(parameter Width = 8)(input logic clk, rst_n);
     logic [$clog2(Width)-1:0] fifo_cnt;
 
     //we need intf signal for both driver and monitor
-    //for which signals of inputs to driver becomes output signals to monitor and viceversa
-    clocking driver_cb @(posedge clk);
-        //default input #1 output #1;
-        output data_in;
-        output wr_en,rd_en;
-        input full,empty;
-        input data_out;
+    //for which signals of inputs to driver becomes output signals to monitor and vice-versa
+//     clocking driver_cb @(posedge clk);
+//         //default input #1 output #1;
+//         output data_in;
+//         output wr_en,rd_en;
+//         input full,empty;
+//         input data_out;
+//     endclocking
+  	clocking monitor_cb @(posedge clk);
+        default input #1ps output #1ps; // Sample just before the edge
+        input wr_en, rd_en, data_in, data_out, empty, full;
     endclocking
 
-    clocking monitor_cb @(posedge clk);
-        //default input #1 output #1;
-        input data_in;
-        input wr_en,rd_en;
-        output full,empty;
-        output data_out;
-    endclocking
+   
 
-    //modports are used to define inputs and outputs for the interface signals
-    modport Driver(clocking driver_cb, input clk,rst);//modport for driver
-    modport Monitor(clocking monitor_cb, input clk,rst);//modport for monitor
+//     //modports are used to define inputs and outputs for the interface signals
+//     modport Driver(clocking driver_cb, input clk,rst_n);//modport for driver
+//    // modport Monitor(clocking monitor_cb, input clk,rst_n);//modport for monitor
 
 endinterface
