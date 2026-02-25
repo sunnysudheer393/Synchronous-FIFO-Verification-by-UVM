11cde613e9f6cfc766256bcdf367775babdb7d24
arm7 <199115150+sunnysudheer393@users.noreply.github.com>
2026-02-25 11:47:21 -0500

    Refactor fifo_monitor to use vif.monitor_cb

--- fifo_monitor.svh
+++ fifo_monitor.svh
@@ -1,4 +1,4 @@
-`define MON_IF vif.Monitor.monitor_cb;
+//`define MON_IF vif.Monitor.monitor_cb;
 class fifo_monitor extends uvm_monitor;
     `uvm_component_utils(fifo_monitor)//it receives packets from interface
 
@@ -15,24 +15,28 @@ class fifo_monitor extends uvm_monitor;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        if(!uvm_config_db #(virtual fifo_interface::get(this,"*","vif",vif))) begin
+      	if(!uvm_config_db #(virtual fifo_interface)::get(this,"*","vif",vif)) begin
             `uvm_error("build_phase","No virtual interface specified for this monitor instance");
         end
     endfunction
 
     virtual task run_phase(uvm_phase phase);
         super.run_phase(phase);
-        trans = fifo_seq_item::type_id::create("trans");
         forever begin
-            @(posedge vif.Monitor.clk);
-            wait(`MON_IF.wr_en == 1 || `MON_IF.rd_en == 1)
-                trans.wr_en = `MON_IF.wr_en;
-                trans.rd_en = `MON_IF.rd_en;
-                trans.data_in = `MON_IF.data_in;
+            //wait(`MON_IF.wr_en == 1 || `MON_IF.rd_en == 1)
+          //trans = fifo_seq_item::type_id::create("trans");
+          @(posedge vif.clk);
+          	trans = fifo_seq_item::type_id::create("trans");
+          
+            trans.wr_en = vif.monitor_cb.wr_en;
+            trans.rd_en = vif.monitor_cb.rd_en;
+            trans.data_in = vif.monitor_cb.data_in;
 
-                trans.empty = `MON_IF.empty;
-                trans.full = `MON_IF.full;
-                trans.data_out = `MON_IF.data_out;
+            trans.empty = vif.monitor_cb.empty;
+            trans.full = vif.monitor_cb.full;
+            trans.data_out = vif.monitor_cb.data_out;
+          //$display("Data_out from DUT: %0d", trans.data_out);
+          	
             mon_port.write(trans);//calls scoreboard write method
         end
     endtask
