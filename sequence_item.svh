30cb638db69308cdf132c60d46d9a9417b5a4464
arm7 <199115150+sunnysudheer393@users.noreply.github.com>
2026-02-25 11:50:34 -0500

    Refactor fifo_seq_item with default values
    Updated fifo_seq_item class to include default values for data_in, wr_en, and rd_en.

--- sequence_item.svh
+++ sequence_item.svh
@@ -1,19 +1,19 @@
 `include "uvm_macros.svh"
 
-class fifo_seq_item extends uvm_sequence_item #(parameter int Width = 8);
+class fifo_seq_item extends uvm_sequence_item();
     `uvm_object_utils(fifo_seq_item)
 
     //contain data and control signals which need to be randomized and declared as rand/randc
-    rand bit[Width-1:0]data_in;
-    rand bit wr_en;
-    rand bit rd_en;
+  rand bit[7:0]data_in='0;
+    rand bit wr_en=0;
+    rand bit rd_en=0;
     //this packet utilized in sequence class for randomization and started in run phase of the test class
 
     bit full;
     bit empty;
-    bit [Width-1:0]data_out;
-
-    virtual function automatic void new(string name = "fifo_seq_item");
+    bit [7:0]data_out;
+  
+    function new(string name="fifo_seq_item");
         super.new(name);
     endfunction
 endclass
